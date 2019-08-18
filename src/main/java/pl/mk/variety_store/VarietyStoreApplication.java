03d839174b259a041cfde032916e070d55aa7994
Mariusz Kowalczuk <lordmer@gmail.com>
2019-08-18 20:11:28 +0200

    initial commit

---
+++ src/main/java/pl/mk/variety_store/VarietyStoreApplication.java
@@ -0,0 +1,13 @@
+package pl.mk.variety_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarietyStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarietyStoreApplication.class, args);
+    }
+
+}
