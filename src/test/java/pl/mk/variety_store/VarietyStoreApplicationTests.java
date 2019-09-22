63ba6f8b58c193da440bd0cb847ece3e59d80a65
Mariusz Kowalczuk <lordmer@gmail.com>
2019-09-23 00:38:18 +0200

    fix

--- src/test/java/pl/mk/variety_store/VarietyStoreApplicationTests.java
+++ src/test/java/pl/mk/variety_store/VarietyStoreApplicationTests.java
@@ -8,9 +8,43 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VarietyStoreApplicationTests {
+    public void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
+    }
 
     @Test
     public void contextLoads() {
+
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
